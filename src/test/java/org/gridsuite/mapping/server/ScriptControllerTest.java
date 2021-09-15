ccc1dd67017d556ccff6897931a5b68a5da4425c
SCALBERT Mathieu <mathieu.scalbert@rte-france.com>
2021-09-15 10:42:00 +0200

    fix: Add Branch import to output script

--- src/test/java/org/gridsuite/mapping/server/ScriptControllerTest.java
+++ src/test/java/org/gridsuite/mapping/server/ScriptControllerTest.java
@@ -23,9 +23,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -116,7 +114,7 @@ public class ScriptControllerTest {
     }
 
     String scriptOutput(String scriptName, String parentName) {
-        return "{\"name\":\"" + scriptName + "\",\"parentName\":\"" + parentName + "\",\"script\":\"/**\n * Copyright (c) 2021, RTE (http://www.rte-france.com)\n * This Source Code Form is subject to the terms of the Mozilla Public\n * License, v. 2.0. If a copy of the MPL was not distributed with this\n * file, You can obtain one at http://mozilla.org/MPL/2.0/.\n */\n\nimport com.powsybl.iidm.network.Generator\nimport com.powsybl.dynawaltz.automatons.CurrentLimitAutomaton\n\nfor (Generator equipment : network.generators) {\n          if (equipment.id.equals(\\\"test\\\") && equipment.minP > 3.000000 && [\\\"HYDRO\\\", \\\"OTHERS\\\"].contains(equipment.energySource) && equipment.voltageRegulatorOn != true) {\n                 GeneratorFourWindings {\n                     staticId equipment.id\n                     parameterSetId  \\\"GSFWPR\\\" + equipment.id\n                 }\n    }\n\n    OmegaRef {\n        generatorDynamicModelId equipment.id\n    }\n}\n\n" +
+        return "{\"name\":\"" + scriptName + "\",\"parentName\":\"" + parentName + "\",\"script\":\"/**\n * Copyright (c) 2021, RTE (http://www.rte-france.com)\n * This Source Code Form is subject to the terms of the Mozilla Public\n * License, v. 2.0. If a copy of the MPL was not distributed with this\n * file, You can obtain one at http://mozilla.org/MPL/2.0/.\n */\n\nimport com.powsybl.iidm.network.Generator\nimport com.powsybl.dynawaltz.automatons.CurrentLimitAutomaton\nimport com.powsybl.iidm.network.Branch\n\nfor (Generator equipment : network.generators) {\n          if (equipment.id.equals(\\\"test\\\") && equipment.minP > 3.000000 && [\\\"HYDRO\\\", \\\"OTHERS\\\"].contains(equipment.energySource) && equipment.voltageRegulatorOn != true) {\n                 GeneratorFourWindings {\n                     staticId equipment.id\n                     parameterSetId  \\\"GSFWPR\\\" + equipment.id\n                 }\n    }\n\n    OmegaRef {\n        generatorDynamicModelId equipment.id\n    }\n}\n\n" +
                 "CurrentLimitAutomaton {\n" +
                 "     staticId \\\"element_id\\\"\n" +
                 "     dynamicModelId \\\"automaton_model\\\"\n" +
@@ -133,32 +131,32 @@ public class ScriptControllerTest {
 
         // Put data
         mvc.perform(post("/mappings/" + name)
-                .content(mapping(name, modelName))
-                .contentType(APPLICATION_JSON))
+                        .content(mapping(name, modelName))
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk());
 
         // convert to script
         mvc.perform(get("/scripts/from/" + name)
-                .contentType(APPLICATION_JSON))
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
                 .andExpect(content().json(scriptOutput(name + "-script", name), true));
 
         // try to convert unknown script
         mvc.perform(get("/scripts/from/" + "unknown")
-                .contentType(APPLICATION_JSON))
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isNotFound());
 
         // Post a mapping without known model (OK car model not needed yet)
         mvc.perform(post("/mappings/" + name)
-                .content(mapping(name, "unknownModel"))
-                .contentType(APPLICATION_JSON))
+                        .content(mapping(name, "unknownModel"))
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk());
 
         // Try to convert this script
         // try to convert unknown script
         mvc.perform(get("/scripts/from/" + name)
-                .contentType(APPLICATION_JSON))
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isNotFound());
 
     }
@@ -172,49 +170,49 @@ public class ScriptControllerTest {
 
         // Put data
         mvc.perform(post("/mappings/" + mappingName)
-                .content(mapping(mappingName, modelName))
-                .contentType(APPLICATION_JSON))
+                        .content(mapping(mappingName, modelName))
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk());
 
         // convert to script
         mvc.perform(get("/scripts/from/" + mappingName)
-                .contentType(APPLICATION_JSON))
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk());
 
         // Rename data
         mvc.perform(post("/scripts/rename/" + mappingName + "-script/to/" + newName
-        )
-                .contentType(APPLICATION_JSON))
+                )
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk());
 
         // get all data
         mvc.perform(get("/scripts/")
-                .contentType(APPLICATION_JSON))
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
                 .andExpect(content().json("[" + scriptOutput(newName, mappingName) + "]", true));
 
         // Add a new script
         mvc.perform(post("/mappings/" + mappingName)
-                .content(mapping(mappingName, modelName))
-                .contentType(APPLICATION_JSON))
+                        .content(mapping(mappingName, modelName))
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk());
 
         // convert to script
         mvc.perform(get("/scripts/from/" + mappingName)
-                .contentType(APPLICATION_JSON))
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk());
 
         // Fail to rename to existing mapping
         mvc.perform(post("/scripts/rename/" + mappingName + "-script/to/" + newName
-        )
-                .contentType(APPLICATION_JSON))
+                )
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isConflict());
 
         // Fail to copy from missing mapping
         mvc.perform(post("/scripts/rename/NotUsed/to/AnyScript"
-        )
-                .contentType(APPLICATION_JSON))
+                )
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isNotFound());
 
     }
@@ -228,49 +226,49 @@ public class ScriptControllerTest {
 
         // Put data
         mvc.perform(post("/mappings/" + mappingName)
-                .content(mapping(mappingName, modelName))
-                .contentType(APPLICATION_JSON))
+                        .content(mapping(mappingName, modelName))
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk());
 
         // convert to script
         mvc.perform(get("/scripts/from/" + mappingName)
-                .contentType(APPLICATION_JSON))
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk());
 
         // Copy data
         mvc.perform(post("/scripts/copy/" + mappingName + "-script/to/" + copyName
-        )
-                .contentType(APPLICATION_JSON))
+                )
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk());
 
         // get all data
         mvc.perform(get("/scripts/")
-                .contentType(APPLICATION_JSON))
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
                 .andExpect(content().json("[" + scriptOutput(mappingName + "-script", mappingName) + ", " + scriptOutput(copyName, mappingName) + "]", true));
 
         // Add a new script
         mvc.perform(post("/mappings/" + mappingName)
-                .content(mapping(mappingName, modelName))
-                .contentType(APPLICATION_JSON))
+                        .content(mapping(mappingName, modelName))
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk());
 
         // convert to script
         mvc.perform(get("/scripts/from/" + mappingName)
-                .contentType(APPLICATION_JSON))
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk());
 
         // Fail to copy to existing mapping
         mvc.perform(post("/scripts/copy/" + mappingName + "-script/to/" + copyName
-        )
-                .contentType(APPLICATION_JSON))
+                )
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isConflict());
 
         // Fail to copy from missing mapping
         mvc.perform(post("/scripts/copy/NotUsed/to/AnyMapping"
-        )
-                .contentType(APPLICATION_JSON))
+                )
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isNotFound());
 
     }
@@ -282,13 +280,13 @@ public class ScriptControllerTest {
 
         // Put data
         mvc.perform(post("/scripts/" + name)
-                .content(simpleScript)
-                .contentType(APPLICATION_JSON))
+                        .content(simpleScript)
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk());
 
         // Get Data
         mvc.perform(get("/scripts/")
-                .contentType(APPLICATION_JSON))
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
                 .andExpect(content().json("[" + simpleScript + "]", true));
@@ -299,7 +297,7 @@ public class ScriptControllerTest {
 
         // Get Data
         mvc.perform(get("/scripts/")
-                .contentType(APPLICATION_JSON))
+                        .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
                 .andExpect(content().json("[]", true));
