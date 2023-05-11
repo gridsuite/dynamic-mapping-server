ee5e874d332f8d206658518a62f47cbda45815be
Thang PHAM <phamthang37@gmail.com>
2023-05-11 12:51:29 +0200

    remove unused import

--- src/main/java/org/gridsuite/mapping/server/dto/automata/CurrentLimitAutomaton.java
+++ src/main/java/org/gridsuite/mapping/server/dto/automata/CurrentLimitAutomaton.java
@@ -6,7 +6,6 @@
  */
 package org.gridsuite.mapping.server.dto.automata;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
