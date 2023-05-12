fdb03f8c8b87fcf2763a78438cecac305325dbc0
Thang PHAM <phamthang37@gmail.com>
2023-05-12 09:04:57 +0200

    correct staticId position in exported groovy

--- src/test/java/org/gridsuite/mapping/server/ScriptControllerTest.java
+++ src/test/java/org/gridsuite/mapping/server/ScriptControllerTest.java
@@ -162,9 +162,9 @@ public class ScriptControllerTest {
     String scriptOutput(String scriptName, String parentName) {
         return "{\"name\":\"" + scriptName + "\",\"parentName\":\"" + parentName + "\",\"script\":\"/**\n * Copyright (c) 2021, RTE (http://www.rte-france.com)\n * This Source Code Form is subject to the terms of the Mozilla Public\n * License, v. 2.0. If a copy of the MPL was not distributed with this\n * file, You can obtain one at http://mozilla.org/MPL/2.0/.\n */\n\nimport com.powsybl.iidm.network.Generator\nimport com.powsybl.dynawaltz.models.automatons.CurrentLimitAutomaton\nimport com.powsybl.iidm.network.Branch\n\nfor (Generator equipment : network.generators) {\n          if (equipment.id.equals(\\\"test\\\") && equipment.minP > 3.000000 && [\\\"HYDRO\\\", \\\"OTHERS\\\"].contains(equipment.energySource) && equipment.voltageRegulatorOn != true) {\n                 GeneratorFourWindings {\n                     staticId equipment.id\n                     parameterSetId  \\\"GSFWPR\\\" + equipment.id\n                 }\n    }\n}\n\n" +
                 "CurrentLimitAutomaton {\n" +
-                "     staticId \\\"element_id\\\"\n" +
                 "     dynamicModelId \\\"automaton_model_element_id\\\"\n" +
                 "     parameterSetId \\\"automaton_model\\\"\n" +
+                "     staticId \\\"element_id\\\"\n" +
                 "     side Branch.Side.ONE\n" +
                 "}\",\"current\": true, \"parametersFile\": null}";
     }
