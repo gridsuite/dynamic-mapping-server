10f7efcc259f83b2579848b6f8c8bd898de83f5f
Hugo Marcellin <hugo.marcelin@rte-france.com>
2025-12-03 11:49:57 +0100

    licence

--- src/test/java/org/gridsuite/mapping/server/error/DynamicMappingExceptionHandlerTest.java
+++ src/test/java/org/gridsuite/mapping/server/error/DynamicMappingExceptionHandlerTest.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.gridsuite.mapping.server.error;
 
 import com.powsybl.ws.commons.error.PowsyblWsProblemDetail;
@@ -10,7 +16,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.gridsuite.mapping.server.error.DynamicMappingErrorBusinessCode.DELETE_FILTER_ERROR;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class DynamicMappingExceptionHandlerTest {
+/**
+ * @author Hugo Marcellin <hugo.marcelin at rte-france.com>
+ */
+class DynamicMappingExceptionHandlerTest {
     private DynamicMappingExceptionHandler handler;
 
     @BeforeEach
