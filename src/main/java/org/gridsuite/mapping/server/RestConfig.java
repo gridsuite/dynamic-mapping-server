5fd2dac83f64b785a0524e0942b96a791e49a0e4
Thang PHAM <phamthang37@gmail.com>
2026-06-15 10:23:50 +0200

    Add Primary Bean ObjectMapper
    Signed-off-by: Thang PHAM <phamthang37@gmail.com>

--- src/main/java/org/gridsuite/mapping/server/RestConfig.java
+++ src/main/java/org/gridsuite/mapping/server/RestConfig.java
@@ -16,6 +16,7 @@ import com.fasterxml.jackson.databind.ser.BeanSerializerModifier;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.client.RestTemplate;
 
@@ -35,6 +36,12 @@ public class RestConfig {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
         return restTemplateBuilder.build();
+    }
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+        return builder.build();
     }
 
     @Bean(EXPORT_MAPPING_OBJECT_MAPPER_BEAN)
