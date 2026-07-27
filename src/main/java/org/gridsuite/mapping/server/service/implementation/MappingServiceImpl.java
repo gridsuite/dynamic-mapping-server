ab502353cacaa387eee02bb31ca1473534f915da
Thang PHAM <phamthang37@gmail.com>
2026-07-27 23:23:55 +0200

    Favorite mappings via config server
    Signed-off-by: Thang PHAM <phamthang37@gmail.com>

--- src/main/java/org/gridsuite/mapping/server/service/implementation/MappingServiceImpl.java
+++ src/main/java/org/gridsuite/mapping/server/service/implementation/MappingServiceImpl.java
@@ -83,7 +83,18 @@ public class MappingServiceImpl implements MappingService {
     @Transactional(readOnly = true)
     @Override
     public List<InputMapping> getMappingList(List<UUID> mappingIds) {
-        List<MappingEntity> mappingEntities = CollectionUtils.isEmpty(mappingIds) ? mappingRepository.findAll() : mappingRepository.findAllById(mappingIds);
+        List<MappingEntity> mappingEntities;
+        if (CollectionUtils.isEmpty(mappingIds)) {
+            mappingEntities = mappingRepository.findAll();
+        } else {
+            // find and keep the same order as mappingIds
+            Map<UUID, MappingEntity> mappingEntityById = mappingRepository.findAllById(mappingIds)
+                    .stream().collect(Collectors.toMap(MappingEntity::getId, mappingEntity -> mappingEntity));
+            mappingEntities = mappingIds.stream()
+                    .map(mappingEntityById::get)
+                    .filter(Objects::nonNull)
+                    .toList();
+        }
 
         List<InputMapping> mappings = mappingEntities.stream().map(InputMapping::new).toList();
 
