551f99ab180bbe4cc73bd5b76021a58c58938032
LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>
2026-09-08 11:25:02 +0200

    remove unused import
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>

--- src/main/java/org/gridsuite/mapping/server/service/client/filter/impl/FilterClientImpl.java
+++ src/main/java/org/gridsuite/mapping/server/service/client/filter/impl/FilterClientImpl.java
@@ -14,7 +14,6 @@ import org.gridsuite.mapping.server.service.client.filter.FilterClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
